674a1e38bb532980317e60307940281235510035
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-03-27 09:15:51 -0400

    title vectorX

--- src/components/Target/TitleVectorX.jsx
+++ src/components/Target/TitleVectorX.jsx
@@ -6,16 +6,22 @@ export default function TitleVectorX({
     upperTitle = '',
     mainTitle = '',
     upperTitleSize = '',
-    mainTitleSize = ''
+    mainTitleSize = '',
+    upperTitleWeigth = 'font-semibold',
+    mainTitleWeigth = 'font-semibold',
+    upperTitleColor = 'text-purple-200',
+    mainTitleColor = 'text-neutrals-00',
+    upperTitleFont = 'font-baloo',
+    mainTitleFont = 'font-baloo',
 }) {
     return (
         <div className={`${width} ${height} flex flex-row justify-between border-blue-500 shadow-md`}>
             <div className='w-full h-full flex flex-col content-between' >
-                <div className='mb-[4px] w-[320px] h-[12px]' >
-                    <p className={`${upperTitleSize} font-semibold text-purple-200 font-baloo`}>{upperTitle}</p>
+                <div className='mb-[4px] w-full h-[12px]' >
+                    <p className={`${upperTitleSize} ${upperTitleWeigth} ${upperTitleColor} ${upperTitleFont}`}>{upperTitle}</p>
                 </div>
-                <div className='w-[320px] h-[23px]' >
-                    <p className={`${mainTitleSize} font-semibold text-white font-baloo`}>{mainTitle}</p>
+                <div className='w-full h-[23px]' >
+                    <p className={`${mainTitleSize} ${mainTitleWeigth} ${mainTitleColor} ${mainTitleFont}`}>{mainTitle}</p>
                 </div>
             </div>
             <div className='w-[14px] h-[14px] flex flex-col justify-center items-center' >

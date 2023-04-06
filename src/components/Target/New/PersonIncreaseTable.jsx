ad91c19af1be2b7d40ec5cea3173ef0e1ec40abc
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-04-06 11:20:20 -0400

    added personal table

--- src/components/Target/New/PersonIncreaseTable.jsx
+++ src/components/Target/New/PersonIncreaseTable.jsx
@@ -1,6 +1,8 @@
-const PersonIncreaseTable = ({ children }) => {
+const PersonIncreaseTable = ({ children, tailwindStyles }) => {
 	return (
-		<div className='flex flex-col justify-between w-[309px] gap-2 pb-2 px-[2px]'>
+		<div
+			className={`flex flex-col justify-between w-[309px] gap-2 pb-2 px-[2px] ${tailwindStyles}`}
+		>
 			{children}
 		</div>
 	);

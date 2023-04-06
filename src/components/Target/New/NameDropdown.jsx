51d207238e81ac70eb656ae18deaeffcbe3c7891
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-04-06 11:19:54 -0400

    modified some styles of name dropdown

--- src/components/Target/New/NameDropdown.jsx
+++ src/components/Target/New/NameDropdown.jsx
@@ -1,14 +1,18 @@
 import Dropdown from '@/components/Target/Dropdown';
 
-
-const NameDropdown = ({ title = 'Salesperson' }) => {
+const NameDropdown = ({
+	title = 'Salesperson',
+	dropDownText = 'Top 3',
+	width = 'w-[244px]',
+	margin = 'mb-[20px]',
+}) => {
 	return (
-		<div className='w-[244px] h-[42px] flex justify-between text-center items-center mb-[20px]'>
-			<h4 className='font-baloo font-bold text-white text-[24px]'>
-				{title}
-			</h4>
+		<div
+			className={`${width} h-[42px] flex justify-between text-center items-center ${margin}`}
+		>
+			<h4 className='font-baloo font-bold text-white text-[24px]'>{title}</h4>
 			<div className='mr-[18px]'>
-				<Dropdown name='Top 3' hight='h-[42px]' width='w-[87px]' />
+				<Dropdown name={`${dropDownText}`} hight='h-[42px]' width='w-[87px]' />
 			</div>
 		</div>
 	);

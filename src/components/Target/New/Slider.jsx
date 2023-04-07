20ffb1891a646442446fe12264519f382a2ecd7f
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-04-07 06:39:11 -0400

    added new slider

--- src/components/Target/New/Slider.jsx
+++ src/components/Target/New/Slider.jsx
@@ -2,22 +2,26 @@
 function Slider({
     sliderwidth = 'w-[177px]',
     sliderheight = 'h-1',
-    slidervalue = '',
+    slidervalue = 'w-[100px]',
     slotheight = 'h-0.5',
     slotwidth = 'w-[177px]',
+    slotbg = 'bg-[#05050F]',
     sliderbg = 'bg-[#FFFFFF]',
     targetValue = 'left-2/3',
+    showBall = true
 }) {
     return (
         <div>
-            <div className={`relative mt-0 ${sliderwidth} h-[24px] flex items-center`}>
-                <div className={`z-0 relative bg-[#05050F] ${slotwidth} ${slotheight} rounded flex items-center`}>
+            <div className={`relative mt-0 ${slotwidth} h-[24px] flex items-center`}>
+                <div className={`z-0 relative ${slotbg} ${slotwidth} ${slotheight} rounded flex items-center`}>
                 </div>
                 {/* ball */}
-                <div className={`z-20 absolute ${targetValue} w-3 h-3 border-[2px] border-[#E5E8EF] bg-[#3E4761] rounded-full flex items-center justify-center`}>
-                    <div className='z-40 w-1 h-1 bg-[#E5E8EF] rounded-full'></div>
-                </div>
-                <div className={`z-10 absolute left-0 ${slidervalue} ${sliderheight} ${sliderbg} rounded-full`}></div>
+                {showBall && (
+                    <div className={`z-20 absolute ${targetValue} w-3 h-3 border-[2px] border-[#E5E8EF] bg-[#3E4761] rounded-full flex items-center justify-center`}>
+                        <div className='z-40 w-1 h-1 bg-[#E5E8EF] rounded-full'></div>
+                    </div>
+                )}
+                <div className={`z-10 absolute left-0 ${sliderwidth} ${slidervalue} ${sliderheight} ${sliderbg} rounded-full`}></div>
             </div>
         </div>
     )

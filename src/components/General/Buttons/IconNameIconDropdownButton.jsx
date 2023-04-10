d0ad0446378320d138155359caa200e1db8fb2d8
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-04-10 18:23:10 -0400

    changed styles of dropdown button

--- src/components/General/Buttons/IconNameIconDropdownButton.jsx
+++ src/components/General/Buttons/IconNameIconDropdownButton.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
-
 import ArrowDownIcon from '@/components/General/Icons/ArrowDownIcon';
 import Decision from '@/components/General/Icons/Decision';
 
 export default function IconNameIconDropdownButton({
 	iconName = 'Home',
 	width = 'w-[254px]',
-	heigth = 'h-[50px]',
+	height = 'h-[50px]',
 	text = 'Select Department',
+	borderRadius='rounded-xl',
+	background = 'bg-neutrals-800',
+	border = 'border border-[2px] border-neutrals-900',
+	backgroundHover = 'hover:bg-neutrals-900 transition duration-300 delay-150 hover:delay-300',
+	borderHover = 'hover:border-purple-600',
+	
 }) {
 	const [showItems, setShowItems] = useState(false);
 
@@ -15,10 +20,10 @@ export default function IconNameIconDropdownButton({
 		<div className='relative'>
 			<button
 				onClick={() => setShowItems(!showItems)}
-				className={`flex flex-row py-[12px] px-[14px] items-center ${width} ${heigth} justify-between ${showItems
+				className={`flex flex-row py-[12px] px-[14px] items-center justify-between ${border} ${width} ${height} ${background} ${borderRadius} ${backgroundHover} ${borderHover} ${showItems
 					? 'border-primary-purple-600 bg-primary-purple-600'
 					: 'bg-[#05081E] border-2 border-[#212A43]'
-					} transition-colors shadow-[0px_13px_25px_-13px_rgba(0, 0, 0, 0.48)] rounded-[10px] hover:border-primary-purple-600 hover:bg-[#161A3E]`}
+					} transition-colors shadow-[0px_13px_25px_-13px_rgba(0, 0, 0, 0.48)]`}
 			>
 				<Decision name={iconName} />
 				<span className='flex flex-align-center font-[600] font-baloo text-[15px] leading-[26px] text-[#EEECF1]'>

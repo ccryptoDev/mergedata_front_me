b09f6971a120b776896123b9269909538c9b0a8a
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-04-20 06:24:23 -0400

    puaseIcon

---
+++ src/components/General/Icons/PauseIcon.jsx
@@ -0,0 +1,11 @@
+
+export default function PauseIcon() {
+    return (
+        <div className='flex flex-row gap-x-[1.56px] items-center justify-center'>
+            <div className='w-[6.22px] h-[14px] border bg-[#FFFFFF] rounded-[3px]'>
+            </div>
+            <div className='w-[6.22px] h-[14px] border rounded-[3px]'>
+            </div>
+        </div>
+    )
+}

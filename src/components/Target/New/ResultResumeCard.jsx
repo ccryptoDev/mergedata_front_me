d45564f28f27d7c632dd587e8432d3e4a2bc68b9
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-04-24 10:38:12 -0400

    resume card

--- src/components/Target/New/ResultResumeCard.jsx
+++ src/components/Target/New/ResultResumeCard.jsx
@@ -6,24 +6,30 @@ export default function ResultResumeCard() {
         <div className='w-[317px] h-[427px] bg-[#05081E] flex flex-col items-start px-3 gap-y-3'>
             <div className='w-[293px] h-8 flex flex-row items-center justify-between'>
                 <div className='w-[209px] h-[32px] flex flex-row border700 rounded-[6px] bg-neutrals-800 items-center justify-between border border-neutrals-900 px-4'>
-                    <p className='font-semibold text-base text-[#EEECF1]  font-baloo'>Search</p>
+                    <p className='font-semibold text-base text-[#EEECF1]  font-baloo'>
+                        Search
+                    </p>
                     <Decision name='Lupe' />
                 </div>
                 <div className='gap-x-[4px] flex flex-row'>
                     <div className='w-9 h-8 rounded-[6px] bg-neutrals-800 border border-neutrals-900 flex items-center justify-center'>
-                        <Decision name='Filter' />
+                        <div className='flex items-center justify-center w-[10px] h-[12px]'>
+                            <Decision name='Filter' />
+                        </div>
                     </div>
                     <div className='w-9 h-8 rounded-[6px] bg-neutrals-800 border border-neutrals-900 flex items-center justify-center'>
-                        <Decision name='DoubleArrowVertical' />
+                        <div className='flex items-center justify-center w-[10px] h-[12px]'>
+                            <Decision name='DoubleArrowVertical' />
+                        </div>
                     </div>
                 </div>
             </div>
             <div className='flex flex-col'>
-                <ResultResume className='border' title='Best consultant' qty='5'/>
-                <ResultResume className='border' title='Best seller' qty='22'/>
-                <ResultResume className='border' title='Sales contest' qty='100'/>
-                <ResultResume className='border' title='Consultant contest' qty='12'/>
-                <ResultResume className='border' title='Best consultant' qty='500'/>
+                <ResultResume className='border' title='Best consultant' qty='5' />
+                <ResultResume className='border' title='Best seller' qty='22' />
+                <ResultResume className='border' title='Sales contest' qty='100' />
+                <ResultResume className='border' title='Consultant contest' qty='12' />
+                <ResultResume className='border' title='Best consultant' qty='500' />
             </div>
 
 

58a7b575f3a3e0fd5a5ae699a81588c8a3ad9879
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-05-15 05:00:33 -0400

    Pause Icon

--- src/components/General/Icons/PauseIcon.jsx
+++ src/components/General/Icons/PauseIcon.jsx
@@ -1,8 +1,10 @@
 
-export default function PauseIcon() {
+export default function PauseIcon(
+    fill = 'border-neutrals-00',
+) {
     return (
         <div className='flex flex-row gap-x-[1.56px] items-center justify-center'>
-            <div className='w-[6.22px] h-[14px] border bg-[#FFFFFF] rounded-[3px]'>
+            <div className={`w-[6.22px] h-[14px] border ${fill} bg-[#FFFFFF] rounded-[3px]`}>
             </div>
             <div className='w-[6.22px] h-[14px] border rounded-[3px]'>
             </div>

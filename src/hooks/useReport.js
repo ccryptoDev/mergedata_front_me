23850817a562e75e1bab9724e600a943afb8e51c
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-05-19 11:34:16 -0400

    useReport

---
+++ src/hooks/useReport.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+import useAxiosPrivate from '@/hooks/useAxiosPrivate';
+
+const useReport = () => {
+	const getReportPath = '/Report/GetReport';
+	const [loading, setLoading] = useState(false);
+	const axiosPrivateReporter = useAxiosPrivate('reporter');
+
+	const getReports = async (
+		reportInfo = { reportId: 1, clientId: 1, storeId: 1, period: '2023-04' },
+	) => {
+		setLoading(true);
+		try {
+			const response = await axiosPrivateReporter.post(getReportPath, 0, {
+				params: {
+					reportId: reportInfo.reportId,
+					clientId: reportInfo.clientId,
+					storeId: reportInfo.storeId,
+					period: reportInfo.period,
+				},
+			});
+			if (response.data.succeeded) {
+				setLoading(false);
+				return response.data.data;
+			}
+		} catch (error) {
+			console.log(error);
+		}
+		setLoading(false);
+		return null;
+	};
+
+	return {
+		loading,
+		getReports,
+	};
+};
+
+export { useReport };

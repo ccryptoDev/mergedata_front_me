7559d2ed17f512a5db3847ac0a65811ed3cfcef4
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-05-30 13:07:04 -0400

    cancel and next button

--- src/components/Target/CancelAndNextButton.jsx
+++ src/components/Target/CancelAndNextButton.jsx
@@ -3,23 +3,36 @@ import TextIconButton from '@/components/General/Buttons/TextIconButton';
 
 const CancelAndNextButton = () => {
     return (
-        <div className='gap-x-3.5 bg-[#202449] w-[431px] h-[58px] flex flex-row justify-center items-center'>
-            <TextIconButton 
-          	    text = 'Cancel' 
-                iconLeft = '' 
-                iconRight = '' 
-                background='bg-[#202449]' 
-                backgroundHover= 'hover:bg-[#FFFFFF]' 
-                textColor='text-[#563CC7]'/>
-            <TextIconButton 
-                text = 'Next' 
-                iconLeft = '' 
-                iconRight = 'RightArrow' 
-                background='bg-[#6043DD]' 
-                backgroundHover='hover:bg-indigo-700' 
-                textColor='text-[#FFFFFF]'/>
+        <div className='gap-x-3.5 bg-transparent w-auto h-auto flex flex-row justify-center items-center'>
+            <TextIconButton
+                text='Cancel'
+                iconLeft=''
+                iconRight=''
+                background='bg-[#05081E]'
+                backgroundHover='hover:bg-[#FFFFFF]'
+                textColor='text-[#563CC7]'
+                width='w-[134px]'
+                height='h-[56px]'
+                textSize = 'text-lg'
+                textColorHover = ''
+            />
+            <TextIconButton
+                text='Next'
+                iconLeft=''
+                iconRight='RightArrow'
+                background='bg-[#6043DD]'
+                backgroundHover='hover:bg-indigo-700'
+                textColor='text-[#FFFFFF]' 
+                width='w-[134px]'
+                height='h-[56px]'
+                textSize = 'text-lg'
+                textColorHover = ''
+            />
+            
         </div>
     );
 };
 
 export default CancelAndNextButton;
+
+   

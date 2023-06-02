3a84233d7a62b5f90b0431679e061061d4d7810b
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-06-02 14:11:01 -0400

    table8, table8 total

--- src/components/Target/New/Table8.jsx
+++ src/components/Target/New/Table8.jsx
@@ -3,94 +3,85 @@ import arrowFatDownIcon from '@/assets/svg/arrow_fat_down_icon.svg';
 import neutral from '@/assets/svg/neutral.svg';
 import arrowUpGreen from '@/assets/svg/arrowupgreen.svg';
 
-const data = [
-    { id: 1, expenses: 'Sales Mgrs Comm & Incentives', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'up', percentage2: '10%' } },
-    { id: 2, expenses: 'Salesperson Mgrs Comm & Inc.', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'down', percentage2: '10%' } },
-    { id: 3, expenses: 'Salaries Salesperson', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'up', percentage2: '10%' } },
-    { id: 4, expenses: 'Delivery Expense', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'up', percentage2: '10%' } },
-    { id: 5, expenses: 'Policy Work', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'steady', percentage2: '10%' } },
-    { id: 6, expenses: 'Used Truck Expense', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'up', percentage2: '10%' } },
-    { id: 7, expenses: 'Interest - Floor Plan - Vehicles', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'down', percentage2: '10%' } },
-    { id: 8, expenses: 'Salaries Adminstration', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'up', percentage2: '10%' } },
-    { id: 9, expenses: 'Nissan Versa', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'down', percentage2: '10%' } },
-];
 
-export default function Table8(shouldReduce = false) {
-
+export default function Table8 ({ shouldReduce = false, report }) {
     let behaviorTextColor = '';
     const behaviorIcon = { neutral };
 
     return (
 
-        <div className="h-[555px]">
+        <div className="h-[555px] overflow-y-auto">
             <table className="min-w-full text-left text-sm font-light ">
                 <thead className="font-medium text-xs leading-[50px] h-10">
                     <tr>
-                        <th scope="col" className="text-purple-50 w-8 text-sm font-bold text-start pl-4">Expenses</th>
-                        <th scope="col" className="text-purple-50 text-sm font-bold text-center pl-2">Total Amount</th>
-                        {shouldReduce.shouldReduce ? null : <th scope="col" className="text-purple-50 text-sm font-bold text-center pl-6">Target</th>}
-                        {shouldReduce.shouldReduce ? null : <th scope="col" className="text-purple-50 text-sm font-bold text-center pl-12">Forecast</th>}
-                        {shouldReduce.shouldReduce ? null : <th scope="col" className="text-purple-50 text-sm font-bold text-center pl-12">Tracking</th>}
-                        <th scope="col" className="text-purple-50 text-sm font-bold text-center pr-5">Variance</th>
+                        <th scope="col" className="text-purple-50 w-8 text-sm font-bold text-start pl-4">{report?.columns[0]}</th>
+                        <th scope="col" className="text-purple-50 text-sm font-bold text-center pl-2">{report?.columns[1]}</th>
+                        {shouldReduce ? null : <th scope="col" className="text-purple-50 text-sm font-bold text-center pl-6">{report?.columns[2]}</th>}
+                        {shouldReduce ? null : <th scope="col" className="text-purple-50 text-sm font-bold text-center pl-12">{report?.columns[3]}</th>}
+                        {shouldReduce ? null : <th scope="col" className="text-purple-50 text-sm font-bold text-center pl-12">{report?.columns[4]}</th>}
+                        <th scope="col" className="text-purple-50 text-sm font-bold text-center pr-5">{report?.columns[5]}</th>
                     </tr>
                 </thead>
                 <tbody className='overflow-y-auto h-[555px]'>
 
                     {
-                        data.map((item) => {
-                            const beHavior = item.variance.behavior;
+                        report?.rows.map((row, index) => {
+                            if (index !== (report.rows.length - 1)) {
+                                const beHavior = row[6].split('::')[0];
 
-                            function scoreColor() {
+                                function scoreColor () {
 
-                                if (beHavior === 'up') {
-                                    behaviorTextColor = 'text-[#5EFF5A]'
+                                    if (beHavior === 'up') {
+                                        behaviorTextColor = 'text-[#5EFF5A]'
+
+                                    }
+                                    else if (beHavior === 'down') {
+                                        behaviorTextColor = 'text-[#FF8181]'
+
+                                    }
+                                    else {
+                                        behaviorTextColor = 'text-[#FFBB0D]'
+                                    }
+                                    return <img src={behaviorIcon} />
 
                                 }
-                                else if (beHavior === 'down') {
-                                    behaviorTextColor = 'text-[#FF8181]'
 
+                                function iconSelect () {
+                                    if (beHavior === 'up') {
+                                        return <img src={arrowUpGreen} />
+                                    }
+                                    else if (beHavior === 'down') {
+                                        return (<img src={arrowFatDownIcon} />)
+                                    }
+                                    else {
+                                        return <img src={neutral} />
+                                    }
                                 }
-                                else {
-                                    behaviorTextColor = 'text-[#FFBB0D]'
-                                }
-                                return <img src={behaviorIcon} />
+                                return (
 
+                                    <tr className={`h-auto border-[6px] border-transparent ${((index) % 2 === 0) ? 'bg-[#151F39]' : ''}`} key={index}>
+                                        <td className="whitespace-nowrap text-neutrals-300 h-auto w-[260px] pl-4 rounded-l-2xl "><div className='text-xl font-bold'>{row[0]}</div></td>
+                                        <td className="whitespace-nowrap text-neutrals-300 h-auto px-4 w-[120px] text-center "><div className=' text-base '>{row[1]}</div></td>
+
+                                        {shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto text-center w-[164px]"><div className='text-base '>{row[2]}</div></td>}
+
+                                        {shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto text-center w-[256px] "><div className='text-base '>{row[3]}</div></td>}
+
+                                        {shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto text-center w-[256px] "><div className='text-base '>{row[4]}</div></td>}
+
+                                        <td className={'whitespace-nowrap text-neutrals-100 text-base rounded-r-2xl'}>
+                                            <div className='h-[48px] flex flex-row justify-center items-center bg-[#000000] brightness-90 rounded-l-2xl rounded-r-2xl px-2' >
+                                                <p className='pr-2 text-base'>{row[5]}</p>
+                                                <p className='pr-1 '>{iconSelect()}</p>
+                                                <p className={`flex items-center ${scoreColor()} ${behaviorTextColor}`}>
+                                                    {row[6].split('::')[1]}
+                                                </p>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
                             }
-
-                            function iconSelect() {
-                                if (beHavior === 'up') {
-                                    return <img src={arrowUpGreen} />
-                                }
-                                else if (beHavior === 'down') {
-                                    return (<img src={arrowFatDownIcon} />)
-                                }
-                                else {
-                                    return <img src={neutral} />
-                                }
-                            }
-                            return (
-                                
-                                <tr className={`h-auto border-[6px] border-transparent ${((item.id) % 2 === 0) ? 'bg-[#151F39]' : ''}`} key={item.id}>
-                                    <td className="whitespace-nowrap text-neutrals-300 h-auto w-[260px] pl-4 rounded-l-2xl "><div className='text-xl font-bold'>{item.expenses}</div></td>
-                                    <td className="whitespace-nowrap text-neutrals-300 h-auto px-4 w-[120px] text-center "><div className=' text-base '>{item.totalAmount}</div></td>
-
-                                    {shouldReduce.shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto text-center w-[164px]"><div className='text-base '>{item.target}</div></td>}
-
-                                    {shouldReduce.shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto text-center w-[256px] "><div className='text-base '>{item.forecast}</div></td>}
-
-                                    {shouldReduce.shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto text-center w-[256px] "><div className='text-base '>{item.tracking}</div></td>}
-
-                                    <td className={'whitespace-nowrap text-neutrals-100 text-base rounded-r-2xl'}>
-                                        <div className='h-[48px] flex flex-row justify-center items-center bg-[#000000] brightness-90 rounded-l-2xl rounded-r-2xl px-2' >
-                                            <p className='pr-2 text-base'>{item.variance.percentage1}</p>
-                                            <p className='pr-1 '>{iconSelect()}</p>
-                                            <p className={`flex items-center ${scoreColor()} ${behaviorTextColor}`}>
-                                                {item.variance.percentage2}
-                                            </p>
-                                        </div>
-                                    </td>
-                                </tr>
-                            )
+                            return null;
                         })
                     }
                 </tbody>
--- src/components/Target/New/Table8Total.jsx
+++ src/components/Target/New/Table8Total.jsx
@@ -2,24 +2,20 @@ import arrowFatDownIcon from '@/assets/svg/arrow_fat_down_icon.svg';
 import neutral from '@/assets/svg/neutral.svg';
 import arrowUpGreen from '@/assets/svg/arrowupgreen.svg';
 
-const data = [
-    { id: 1, expenses: 'Total', totalAmount: '$46.273', target: 32, forecast: 32, tracking: 15, variance: { percentage1: '21%', behavior: 'up', percentage2: '10%' } },
-]
-export default function Table7Total(shouldReduce = false) {
 
+export default function Table8Total ({ shouldReduce = false, report }) {
     let behaviorTextColor = '';
     const behaviorIcon = { neutral };
 
     return (
-
-        <div className={`h-[60px] flex flex-col ${!shouldReduce.shouldReduce ? 'w-[1363px]' : 'w-[550px]'}`}>
+        <div className={`h-[60px] flex flex-col ${!shouldReduce ? 'w-[1363px]' : 'w-[550px]'}`}>
             <table className="whitespace-nowrap h-[60px] text-left text-sm font-light bg-[#505A78] rounded-2xl">
                 <tbody className='overflow-y-auto h-auto pb-6'>
                     {
-                        data.map((item) => {
-                            const beHavior = item.variance.behavior;
+                        report?.rows.slice(-1).map((total, index) => {
+                            const beHavior = total[6].split('::')[0];
 
-                            function scoreColor() {
+                            function scoreColor () {
 
                                 if (beHavior === 'up') {
                                     behaviorTextColor = 'text-[#5EFF5A]'
@@ -36,7 +32,7 @@ export default function Table7Total(shouldReduce = false) {
 
                             }
 
-                            function iconSelect() {
+                            function iconSelect () {
                                 if (beHavior === 'up') {
                                     return <img src={arrowUpGreen} />
                                 }
@@ -49,22 +45,22 @@ export default function Table7Total(shouldReduce = false) {
                             }
                             return (
 
-                                <tr className='h-auto rounded-x-2xl border-separate border-spacing-2' key={item.id}>
-                                    <td className="whitespace-nowrap text-neutrals-300 h-auto w-[336px] pl-10 rounded-l-2xl "><div className='text-lg font-bold text-neutrals-00'>{item.expenses}</div></td>
+                                <tr className='h-auto rounded-x-2xl border-separate border-spacing-2' key={index}>
+                                    <td className="whitespace-nowrap text-neutrals-300 h-auto w-[336px] pl-10 rounded-l-2xl "><div className='text-lg font-bold text-neutrals-00'>{total[0]}</div></td>
 
-                                    <td className={`whitespace-nowrap text-neutrals-300 h-auto px-3 text-center ${shouldReduce.shouldReduce ? 'w-[97px]' : 'w-[120px]'}`}><div className=' text-base '>{item.totalAmount}</div></td>
+                                    <td className={`whitespace-nowrap text-neutrals-300 h-auto px-3 text-center ${shouldReduce ? 'w-[97px]' : 'w-[120px]'}`}><div className=' text-base '>{total[1]}</div></td>
 
-                                    {shouldReduce.shouldReduce ? null : <td className=" whitespace-nowrap text-neutrals-300 h-auto w-[164px] text-center"><div className='text-base '>{item.target}</div></td>}
-                                    
-                                    {shouldReduce.shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto w-[255px] text-center"><div className='text-base '>{item.forecast}</div></td>}
-                                    
-                                    {shouldReduce.shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto w-[256px] text-center"><div className='text-base '>{item.tracking}</div></td>}
+                                    {shouldReduce ? null : <td className=" whitespace-nowrap text-neutrals-300 h-auto w-[164px] text-center"><div className='text-base '>{total[2]}</div></td>}
 
-                                    <td className={`whitespace-nowrap text-neutrals-100 h-12 text-base flex flex-row flex-nowrap items-center justify-center mt-[5px] rounded-2xl bg-[#151F39] brightness-90 ${shouldReduce.shouldReduce ? 'w-[100px]' : 'w-[206px]'}`}>
-                                        <p className='pr-2 file:text-base'>{item.variance.percentage1}</p>
+                                    {shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto w-[255px] text-center"><div className='text-base '>{total[3]}</div></td>}
+
+                                    {shouldReduce ? null : <td className="whitespace-nowrap text-neutrals-300 h-auto w-[256px] text-center"><div className='text-base '>{total[4]}</div></td>}
+
+                                    <td className={`whitespace-nowrap text-neutrals-100 h-12 text-base flex flex-row flex-nowrap items-center justify-center mt-[5px] rounded-2xl bg-[#151F39] brightness-90 ${shouldReduce ? 'w-[100px]' : 'w-[206px]'}`}>
+                                        <p className='pr-2 file:text-base'>{total[5]}</p>
                                         <p className='pr-1 '>{iconSelect()}</p>
                                         <p className={`flex items-center ${scoreColor()} ${behaviorTextColor}`}>
-                                            {item.variance.percentage2}
+                                            {total[6].split('::')[1]}
                                         </p>
                                     </td>
 

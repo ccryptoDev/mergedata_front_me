bd522ed1dce134c02f54f98c027df99fc4870ba9
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-06-07 07:45:20 -0400

    Backlink

---
+++ src/components/General/Buttons/BackLink.jsx
@@ -0,0 +1,9 @@
+import Decision from '@/components/General/Icons/Decision'
+
+export default function BackLink() {
+    return (
+        <div className='w-[44px] h-[44px] mt-[3px] ml-[1px] bg-[#6043DD] rounded-full flex justify-center items-center'>
+            <Decision name='LeftArrow' />
+        </div>
+    )
+}

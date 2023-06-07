b22efff8fc00f654e895b4fce0f2dbe8f28354da
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-06-07 07:45:02 -0400

    name Initials component

---
+++ src/components/General/NameInitials.jsx
@@ -0,0 +1,10 @@
+
+export default function NameInitials() {
+    return (
+        <div className='relative w-8 h-8 border border-[#FFFFFF] bg-[#E5E8EF] flex items-center justify-center rounded-full'>
+            <p className='font-bold text-sm text-[#563CC7] rounded-full'>JC</p>
+            <div className='absolute right-0 bottom-0 w-2 h-2 border border-[#FFFFFF] bg-[#60CB82] rounded-full'>
+            </div>
+        </div>
+    )
+}

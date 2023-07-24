bf51b008d9677f938a97e0cdee5a255ede4d0ad6
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-07-24 16:57:33 -0400

    updated generic table

--- src/components/General/GenericTable/GenericTable.jsx
+++ src/components/General/GenericTable/GenericTable.jsx
@@ -1,11 +1,28 @@
+import { useContext } from 'react';
+
 import { increaseStatus } from '@/utils/helperFunctions';
+import Loader from '@components/General/Loader';
+import { useReport } from '@/hooks/useReport';
+import ReportsContext from '@/context/ReportsProvider';
 import './GenericTableStyles.css';
 
-const GenericTable = ({ withTotal = true, columns, rows, types }) => {
-	console.table(rows);
+
+const GenericTable = ({ withTotal = true, columns, rows }) => {
+	const { getDrillDown } = useReport();
+	const { report } = useContext(ReportsContext);
 	const rowsWithoutTotal = withTotal ? rows?.slice(0, -1) : rows;
 	const totalRow = rows?.slice(-1);
-	function varianceCell(cell, index) {
+	const getLine = id => {
+		const drillDown = getDrillDown({
+			level: 1,
+			reportLineId: id,
+			storeId: report?.storeId,
+			period: report?.period,
+		});
+		console.log('drillDown', drillDown);
+	}
+
+	function varianceCell (cell, index) {
 		const { icon, textColor } = increaseStatus(cell.split('::')[0]);
 		return (
 			<td key={index}>
@@ -18,41 +35,69 @@ const GenericTable = ({ withTotal = true, columns, rows, types }) => {
 			</td>
 		);
 	}
+
 	return (
-		<table>
-			<thead>
-				<tr className='table-header'>
-					{columns?.map((column, index) => (
-						<th key={index}>{column}</th>
-					))}
-				</tr>
-			</thead>
-			<tbody>
-				{rowsWithoutTotal?.map((row, index) => (
-					<tr key={index}>
-						{row.map((cell, index) =>
-							typeof cell === 'string' && cell.includes('::') ? (
-								varianceCell(cell, index)
-							) : (
-								<td key={index}>{cell}</td>
-							),
-						)}
-					</tr>
-				))}
-				{withTotal &&
-					totalRow?.slice(-1).map((row, index) => (
-						<tr key={index} className='total-row'>
-							{row.map((cell, index) =>
-								typeof cell === 'string' && cell.includes('::') ? (
-									varianceCell(cell, index)
-								) : (
-									<td key={index}>{cell}</td>
-								),
-							)}
+		<div>
+			{columns ? (
+				<table>
+					<thead>
+						<tr className='table-header'>
+							{columns?.map((column, index) => (
+								<th key={index}>{column}</th>
+							))}
 						</tr>
-					))}
-			</tbody>
-		</table>
+					</thead>
+					<tbody>
+						{rowsWithoutTotal?.map((row, index) => (
+							<tr
+								key={`row-${index}`}
+								onClick={() => { getLine(row); }}
+							>
+								{row.style === 'Title' ? (
+									<td key={`line-description-${index}`} className='title-cell'>
+										{row.name}
+									</td>
+								) : (
+									<td key={`line-description-${index}`}>{row.name}</td>
+								)}
+								{row?.values?.map((cell, index) =>
+									typeof cell === 'string' && cell.includes('::') ? (
+										varianceCell(cell, index)
+									) : (
+										<td key={`cell-${index}`}>{cell}</td>
+									),
+								)}
+							</tr>
+						))}
+						{withTotal &&
+							totalRow?.slice(-1).map((row, index) => (
+								<tr
+									key={`row-total-${index}`}
+									className='total-row'
+									onClick={() => { getLine(row); }}
+								>
+									<td key={`line-description-total-${index}`}>{row.name}</td>
+									{row?.values?.map((cell, index) =>
+										typeof cell === 'string' && cell.includes('::') ? (
+											varianceCell(cell, index)
+										) : (
+											<td key={`cell-total-${index}`}>{cell}</td>
+										),
+									)}
+								</tr>
+							))}
+					</tbody>
+				</table>
+			) : columns && columns.length === 0 ? (
+				<p className='font-public text-sm text-neutrals-00'>
+					There&apos;s no data to show =(
+				</p>
+			) : (
+				<div className='w-full min-h-[550px] flex justify-center items-center'>
+					<Loader size={'large'} />
+				</div>
+			)}
+		</div>
 	);
 };
 

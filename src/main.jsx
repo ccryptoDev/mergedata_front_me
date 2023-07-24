ae9f43f27f34ba7306ecf16d85781c6c124859a6
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-07-24 16:56:44 -0400

    added report provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "flowbite"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'flowbite';
 
-import App from '@/routes/App'
+import App from '@/routes/App';
 import { AuthProvider } from '@/context/AuthProvider';
-import "@/css/global.css"
+import { ReportsProvider } from '@/context/ReportsProvider';
+import '@/css/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<AuthProvider>
+			<ReportsProvider>
+				<App />
+			</ReportsProvider>
+		</AuthProvider>
+	</React.StrictMode>,
+);

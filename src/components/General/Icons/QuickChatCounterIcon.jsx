a610d5f6611622287fc1e43fd9056a5c0d6daa50
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-09-14 05:04:32 -0400

    MD-302

---
+++ src/components/General/Icons/QuickChatCounterIcon.jsx
@@ -0,0 +1,7 @@
+export default function QuickChatCounterIcon() {
+    return (
+        <div className='w-[16px] h-[17px] box-content rounded-full border-[3px] flex items-center justify-center bg-[#907BE7]'>
+            <p className='text-[#FFFFFF] text-[11px] font-baloo font-semibold'>6</p>
+        </div >
+    )
+}

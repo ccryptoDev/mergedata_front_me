9a749ade28f92d25994928dba3bed5f583832b4a
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-09-15 05:01:54 -0400

    MD-247

---
+++ src/components/General/Integrations/AllButton.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import IconNameIconDropdownButton from '@/components/General/Buttons/IconNameIconDropdownButton'
+import AllFilter from './AllFilter'
+
+export default function AllButton() {
+    const [showFilterOptions, setShowFilterOptions] = useState(false)
+    console.log('showMenu?', showFilterOptions)
+
+    return (
+        <div className='relative'>
+            <div onClick={() => setShowFilterOptions(!showFilterOptions)}>
+                <IconNameIconDropdownButton
+                    iconName='Filter'
+                    width='w-[118px]'
+                    height='h-[50px]'
+                    text='All'
+                    borderRadius='rounded-xl'
+                    background='#05081E'
+                    border='border border-neutrals-800'
+                    backgroundColorHover='hover:bg-neutrals-900 transition duration-300 delay-150 hover:delay-300'
+                    borderHover='hover:border-purple-600'
+                />
+            </div>
+            <div className='absolute right-0 top-13'>
+                {showFilterOptions && (
+                    <AllFilter />
+                )}
+            </div>
+        </div>
+
+    )
+}

1e5f08d7c1e4d754225d81cccf7dc78e7b1eb3e5
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-09-18 09:48:16 -0400

    MD-18 (remodified)

---
+++ src/components/General/Integrations/MonthlyTrendGraph.jsx
@@ -0,0 +1,9 @@
+import trendGraph from '@/assets/svg/trendGraph.svg'
+
+export default function MonthlyTrendGraph() {
+  return (
+    <div className='w-[874px] h-[625px]'>
+        <img src={trendGraph} alt='MonthlyTrendGraph' />
+    </div>
+  )
+}

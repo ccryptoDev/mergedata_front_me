ba850f3b1a1c5c9efab59367c6d63c518b9e73d9
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-09-22 07:10:28 -0400

    MD-115

---
+++ src/components/General/MergeChat/ExcelAttachment.jsx
@@ -0,0 +1,10 @@
+import excelAttachment from '@/assets/svg/excelAttachment.svg';
+
+export default function ExcelAttachment() {
+  return (
+    <div className="">
+        <img src={excelAttachment} alt="excel-attachment" />
+        
+    </div>
+  )
+}

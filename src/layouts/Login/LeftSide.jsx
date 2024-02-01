d41c268d8e7c857c35dc73e7e3a489fcdfd9ebe8
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-01-31 23:17:58 -0400

    MD-243

--- src/layouts/Login/LeftSide.jsx
+++ src/layouts/Login/LeftSide.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 import LoginForm from '@/components/Login/LoginForm';
 import mergeDataLogo from '@/assets/svg/mergedata_logo.svg';
+import leftElypseLogin from '@/assets/images/eclipse-min.png';
 import oktaLogo from '@/assets/svg/okta_logo.svg';
 import infoIcon from '@/assets/svg/info_icon.svg';
 
@@ -9,23 +10,30 @@ import infoIcon from '@/assets/svg/info_icon.svg';
 export default function LeftSide ({ width }) {
 	return (
 		<section
-			className={`${width} bg-neutrals-00 rounded-r-3xl flex flex-col justify-between p-16`}
+			className={`md:w-[47%] w-full rounded-r-3xl flex flex-col justify-between p-16`}
 		>
+			<img
+				src={leftElypseLogin}
+				alt='Shape element'
+				className='fixed eclipse-left-login'
+			/>
 			<img
 				src={mergeDataLogo}
 				alt='MergeData logo'
 				className='w-[192px] h-[44px]'
 			/>
 
-			<LoginForm />
+			
+
+			<LoginForm className="z-50" />
 
 			<>
-				<div className='w-[275px] mx-auto '>
+				<div className='w-[275px] mx-auto z-50'>
 					<Link
 						to='/oktaLogin'
-						className='flex justify-center items-center gap-2 text-xs text-neutrals-800'
+						className='flex justify-center items-center gap-2 text-xs text-neutrals-300'
 					>
-						<p>Or Login with</p>
+						<p className='pt-1'>Or Login with</p>
 						<img src={oktaLogo} alt='OKTA logo' />
 					</Link>
 				</div>

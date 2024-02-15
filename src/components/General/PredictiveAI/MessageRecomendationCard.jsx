d6f7a737004b8fe90014f6b8d53c2a7d860e2028
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-02-15 06:20:05 -0400

    MD-131

--- src/components/General/PredictiveAI/MessageRecomendationCard.jsx
+++ src/components/General/PredictiveAI/MessageRecomendationCard.jsx
@@ -1,43 +1,33 @@
 import { useState } from 'react';
+import { useContext } from 'react';
+import { usePage } from '@/hooks/usePage';
+import ReportsContext from '@/context/ReportsProvider';
 import TextInputChat from '@/components/General/PredictiveAI/TextInputChat';
 import Graphs from '@/assets/svg/Graphs.svg'
 import bot3 from '@/assets/svg/bot3.svg'
 import NameInitials from '@/components/General/NameInitials';
 import Decision from '@/components/General/Icons/Decision'
 import TextIconButton from '@/components/General/Buttons/TextIconButton';
+import LeftArrowDirectActionHeart from '@/components/Target/New/LeftArrowDirectActionHeart';
+import { increaseStatus } from '@/utils/helperFunctions';
 
 
 export default function MessageRecomendationCard() {
-
+    const { returnOnePage, moveToPage } = usePage();
+    const { user, storesSelected } = useContext(ReportsContext);
+    const { icon, textColor } = increaseStatus('up');
     const [showFlag, setShowFlag] = useState(false);
 
     return (
         <div className='relative z-10 w-[1070px] h-[786px] bg-[#0D102C] rounded-xl p-6 flex flex-col items-center justify-between gap-y-6'>
-            <div className='z-20 w-[1022px] h-[58px] bg-[#05081E] flex justify-between items-center'>
-                <div className='flex flex-col'>
-                    <div className='w-auto h-[58px] flex flex-row justify-between' >
-                        <div className='w-[44px] h-[44px] mt-[3px] ml-[1px] mr-3 bg-[#6043DD] rounded-full flex justify-center items-center'>
-                            <Decision name='LeftArrow' />
-                        </div>
-                        <div className='flex flex-col'>
-                            <p className='text-[#FFFFFF] text-[12px] leading-[18px]'>Inventory Department</p>
-                            <p className='text-[#FFFFFF] font-bold font-baloo text-[32px] pr-4   bg-[#0D102C] leading-[36px] z-30'>Improve Inventory Supply</p>
-                        </div>
-
-                        <div className='w-auto h-auto flex flex-row'>
-                            <div
-                                onMouseOver={() => setShowFlag(true)}
-                                onMouseLeave={() => setShowFlag(false)}
-                                className='bg-[#161A3E] rounded-[8px] w-[30px] h-[30px] text-[#ffffff] flex flex-row justify-center items-center mt-5 z-20'
-                            >
-                                <Decision name='Heart' />
-                            </div>
-                            <div className={`bg-[#161A3E] w-auto px-3 rounded-r-[10px] h-[30px] text-[#ffffff] flex justify-center items-center mt-5 text-[14px] ml-[-5px] ${showFlag ? 'translate-x-0 opacity-70' : 'translate-x-[-140px] opacity-0'} easy-in-out duration-500 z-10`}>
-                                Add as favorite
-                            </div>
-                        </div>
-
-                    </div>
+            <div className='z-20 w-[63.9em] h-[3.62em] bg-[#05081E] flex justify-between items-center'>
+                <div className=''>
+                    <LeftArrowDirectActionHeart
+                        bgColor='bg-dark-popup-00'
+                        buttonName='Used'
+                        upperName='Targets'
+                        onClick={returnOnePage}
+                    />
                 </div>
                 <div className='w-auto h-[58px] flex justify-center items-center bg-[#05081E] rounded-full gap-x-4'>
                     <TextIconButton

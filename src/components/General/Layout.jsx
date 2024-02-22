7ef8d2989eed15f8e1f5f2e5d6b846176d7f038e
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-02-22 06:17:54 -0400

    MD-160

--- src/components/General/Layout.jsx
+++ src/components/General/Layout.jsx
@@ -1,10 +1,12 @@
 export default function Layout({ children, bgColor = 'bg-dark-popup-00' }) {
 	return (
 		<div
-			className={`w-screen h-screen flex flex-col overflow-hidden relative ${bgColor} justify-between`}
+			className={`w-screen h-screen flex flex-col relative ${bgColor} justify-between overflow-hidden`}
 		>
-			<div className='max-w-[1440px] h-full mx-auto pb-5'>
-				<div className='h-full flex justify-between flex-col'>{children}</div>
+			<div className='max-w-[1440px] h-full mx-auto pb-8 w-full'>
+				<div className='h-full flex justify-between flex-col gap-5'>
+					{children}
+				</div>
 			</div>
 		</div>
 	);

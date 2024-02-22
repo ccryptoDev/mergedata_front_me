fc6ed17fe20eff02436c1af7f7fccf8849fc3b8e
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-02-22 06:17:43 -0400

    MD-166

--- src/components/General/Buttons/IconNameIconDropdownButton.jsx
+++ src/components/General/Buttons/IconNameIconDropdownButton.jsx
@@ -7,12 +7,13 @@ export default function IconNameIconDropdownButton({
 	width = 'w-[254px]',
 	height = 'h-[50px]',
 	text = 'Select Department',
-	borderRadius='rounded-xl',
+	borderRadius = 'rounded-xl',
 	background = 'bg-neutrals-800',
 	border = 'border border-[2px] border-neutrals-900',
 	backgroundHover = 'hover:bg-neutrals-900 transition duration-300 delay-150 hover:delay-300',
 	borderHover = 'hover:border-purple-600',
-	
+	font = 'font-[600] font-baloo text-[15px] leading-[26px] text-[#EEECF1]',
+	activeStyle = 'bg-[#2e2]',
 }) {
 	const [showItems, setShowItems] = useState(false);
 
@@ -21,12 +22,12 @@ export default function IconNameIconDropdownButton({
 			<button
 				onClick={() => setShowItems(!showItems)}
 				className={`flex flex-row py-[12px] px-[14px] items-center justify-between ${border} ${width} ${height} ${background} ${borderRadius} ${backgroundHover} ${borderHover} ${showItems
-					? 'border-primary-purple-600 bg-primary-purple-600'
+					? activeStyle
 					: 'bg-[#05081E] border-2 border-[#212A43]'
 					} transition-colors shadow-[0_13px_25px_-13px_rgba(0,0,0,0.48)]`}
 			>
 				<Decision name={iconName} />
-				<span className='flex flex-align-center font-[600] font-baloo text-[15px] leading-[26px] text-[#EEECF1]'>
+				<span className={`flex flex-align-center ${font}`}>
 					{text}
 				</span>
 				<ArrowDownIcon

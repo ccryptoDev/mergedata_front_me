a89323a10f3d4ea2acfa009f6147b58a9321dae5
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-03-04 23:09:10 -0400

    MD-366

--- src/components/General/Contest/NotificationsCard.jsx
+++ src/components/General/Contest/NotificationsCard.jsx
@@ -1,27 +1,27 @@
-import NotificationGroup from "@/components/General/Contest/NotificationGroup"
+import NotificationGroup from '@/components/General/Contest/NotificationGroup';
 
 export default function NotificationsCard() {
-    return (
-        <div className='w-[317px] h-[692px] bg-[#0D102C] flex justify-center rounded-xl'>
-            <div className='w-[293px] h-[668px] flex flex-col overflow-hidden'>
-                <div className='w-[293px] h-[41px] flex justify-center my-3'>
-                    <div className='w-[277px] h-[41px] bg-[#563CC7] rounded-lg flex flex-row items-center justify-center gap-x-2'>
-                        <p className='text-[13px] text-[#FFFFFF] text-semibold '>Notifications</p>
-                        <div className='w-[26px] h-[20px] border-[3px] border-[#563CC7] border-double  rounded-lg bg-primary-purple-300 flex items-center justify-center'>
-                            <p className='text-[12px] text-[#FFFFFF] font-bold'>99</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='w-[293px]'>
-                    <NotificationGroup contestStatus='1 Days to finishing Game'/>
-                </div>
-                <div className='w-[293px]'>
-                    <NotificationGroup contestStatus='3 Days to finishing Game'/>
-                </div>
-                <div className='w-[293px]'>
-                    <NotificationGroup contestStatus='7 Days to finishing Game'/>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className='w-[23%] h-full bg-[#0D102C] flex rounded-xl px-3 pb-6'>
+			<div className='flex flex-col h-full w-full'>
+				<div className='flex my-3'>
+					<div className='w-full p-3 bg-[#563CC7] rounded-lg flex items-center justify-center gap-x-2'>
+						<p className='text-[13px] text-[#FFFFFF] text-semibold '>
+							Notifications
+						</p>
+						<div className='border-[0.13rem] border-white bg-primary-purple-300 rounded-[6.25rem] px-[0.31rem]'>
+							<p className='text-[0.5625rem] text-[#FFFFFF] font-semibold leading-[17px]'>
+								99
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className='h-[calc(100vh-310px)] overflow-auto flex flex-col justify-between'>
+					<NotificationGroup contestStatus='1 Days to finishing Game' />
+					<NotificationGroup contestStatus='3 Days to finishing Game' />
+					<NotificationGroup contestStatus='7 Days to finishing Game' />
+				</div>
+			</div>
+		</div>
+	);
 }

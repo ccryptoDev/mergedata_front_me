ea269bf0a55295d8ec22d478546ec1ca492cf287
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-03-14 12:28:54 -0400

    ND-141

--- src/pages/TargetPreset/TargetPreset.jsx
+++ src/pages/TargetPreset/TargetPreset.jsx
@@ -1,29 +1,54 @@
-import SharedLayout from '@/components/General/SharedLayout';
+import Header from '@/components/General/Header/Header';
+import Layout from '@/components/General/Layout';
+import Ribbon from '@/components/General/Ribbon/Ribbon';
 import CardAddTargetsPresets from '@/components/Target/CardAddTargetsPresets';
 import CardPreset from '@/components/Target/CardPreset';
-import LeftArrowDirectActionHeart from '@/components/Target/New/LeftArrowDirectActionHeart';
-import { usePage } from '@/hooks/usePage';
 
-function TargetPreset () {
-	const { returnOnePage } = usePage();
+function TargetPreset() {
 	return (
-		<SharedLayout bgColor='bg-dark-background-00'>
-			<LeftArrowDirectActionHeart
-				upperName='Target'
-				buttonName='Preset'
-				onClick={returnOnePage}
-			/>
-			<div className='flex gap-6 mb-7'>
-				<CardPreset />
-				<CardPreset />
-				<CardPreset />
+		<Layout bgColor='bg-[#05081E]'>
+			<Header />
+			<Ribbon withIconBack title={'Targets'} subtitle={'Presets'}></Ribbon>
+			<div className='flex flex-col justify-around items-center h-[calc(100vh-156px)]'>
+				<div className='flex gap-6'>
+					<CardPreset
+						title={"Josh's Optimal Budget"}
+						description={
+							'Navigate automotive finance with precision. Practical insights for effective budgeting.'
+						}
+					/>
+					<CardPreset
+						title={'2023 Budget'}
+						description={
+							'Strategize your finances with precision. Practical insights for effective budgeting.'
+						}
+						isPrivate={true}
+					/>
+					<CardPreset
+						title={'Pessimistic Outlook Budget'}
+						description={
+							'Navigate financial challenges with practical insights. Strategic budgeting for uncertain times.'
+						}
+						isPrivate={true}
+					/>
+				</div>
+				<div className='flex gap-6'>
+					<CardPreset
+						title={'Average for last month'}
+						description={
+							'Assess your financial standing with a practical overview. Evaluate expenses and plan strategically for the future.'
+						}
+					/>
+					<CardPreset
+						title={'Used by Year'}
+						description={
+							'Analyze your annual spending patterns. Gain insights to optimize budgeting and financial planning.'
+						}
+					/>
+					<CardAddTargetsPresets />
+				</div>
 			</div>
-			<div className='flex gap-6'>
-				<CardPreset />
-				<CardPreset />
-				<CardAddTargetsPresets />
-			</div>
-		</SharedLayout>
+		</Layout>
 	);
 }
 

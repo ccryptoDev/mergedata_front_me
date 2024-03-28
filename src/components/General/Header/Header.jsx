a473e80a5e436b7e405560cf2616401e3ffe406f
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-03-28 07:27:19 -0400

    MD-165

--- src/components/General/Header/Header.jsx
+++ src/components/General/Header/Header.jsx
@@ -1,4 +1,3 @@
-import menuButton from '@/assets/svg/menu_button.svg';
 import mergeDataLogo from '@/assets/svg/mergedata_logo.svg';
 import NavBar from '@/components/General/NavBar';
 
@@ -8,12 +7,15 @@ const Header = ({ bgColor = 'bg-dark-popup-00', isHome = false }) => {
 			? 'bg-dark-background-00'
 			: 'bg-dark-popup-00';
 	return (
-		<header className='mt-3 w-full h-[56px] flex justify-between items-center gap-6'>
-			<div className='flex w-[124px] justify-between'>
-				<img src={menuButton} alt='menu button' />
-				<img src={mergeDataLogo} alt='merge data logo' className='w-23 h-8' />
+		<header className='mt-[0.75rem] mx-4 flex justify-between items-center h-[4em] gap-[2.18em] z-50'>
+			<div className='flex w-1/12 justify-between items-center'>
+				<div className='mr-[1em]'>
+					<img src={mergeDataLogo} alt='merge data logo' />
+				</div>
 			</div>
-			{!isHome && <NavBar navBarBgColor={navBarBgColor} />}
+			<div className='w-full '>
+				{!isHome && <NavBar navBarBgColor={navBarBgColor} />}
+			</div>
 		</header>
 	);
 };

c6737a1c2f1088dd11166df752810af8a43d94fe
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-08-11 23:55:30 -0400

    MD-282

---
+++ src/components/General/Icons/PlusIcon.jsx
@@ -0,0 +1,20 @@
+const PlusIcon = () => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='25'
+			height='24'
+			fill='none'
+			viewBox='0 0 25 24'
+		>
+			<path
+				stroke='#EFECFC'
+				strokeLinecap='round'
+				strokeWidth='1.5'
+				d='M12.666 17V7M7.666 12h10'
+			></path>
+		</svg>
+	);
+};
+
+export default PlusIcon;

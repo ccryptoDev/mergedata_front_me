f45b93c05be79a849ac5a9e656d9356b51a162e1
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-08-30 10:15:43 -0400

    direct message component

---
+++ src/components/General/MergeChat/LeftPanel/DirectMessage.jsx
@@ -0,0 +1,13 @@
+import ScrollableList from '@/components/General/MergeChat/LeftPanel/ScrollableList'
+
+export default function DirectMessage({data}) {
+    return (
+      <ScrollableList 
+      title='DirectMessage' 
+      icon=''
+      data={data}
+      />
+  
+    )
+  }
+

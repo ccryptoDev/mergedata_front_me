c3ca4860263dfebc43d64fc6f890ec081421f733
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-11 06:17:03 -0400

    MDF-61

---
+++ src/components/General/MergeChat/RightPanel/Attachment/AttachmentsReceived.jsx
@@ -0,0 +1,13 @@
+
+import AttachmentsReceivedBody from "@/components/General/MergeChat/RightPanel/Attachment/AttachmentsReceivedBody";
+import ChatBodyFooter from '@/components/General/MergeChat/RightPanel/ChatBodyFooter'
+
+
+export default function AttachmentsReceived() {
+  return (
+    <div>
+      <AttachmentsReceivedBody />
+      <ChatBodyFooter />
+    </div>
+  )
+}

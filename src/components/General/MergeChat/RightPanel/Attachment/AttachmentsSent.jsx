fa2ec81621e08924af8111b90256c4f7ecc50ed9
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-11 06:17:27 -0400

    MDF-54

---
+++ src/components/General/MergeChat/RightPanel/Attachment/AttachmentsSent.jsx
@@ -0,0 +1,12 @@
+import AttachmentsSentBody from '@/components/General/MergeChat/RightPanel/Attachment/AttachmentsSentBody'
+import ChatBodyFooter from '@/components/General/MergeChat/RightPanel/ChatBodyFooter'
+
+
+export default function AttachmentsSent() {
+  return (
+    <div>
+      <AttachmentsSentBody />
+      <ChatBodyFooter />
+    </div>
+  )
+}

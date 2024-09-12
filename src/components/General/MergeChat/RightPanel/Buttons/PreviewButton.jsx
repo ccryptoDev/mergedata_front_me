8f7dd8344e0a6954758a45c6f22c91563e86df84
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-12 07:07:58 -0400

    MDF-73

---
+++ src/components/General/MergeChat/RightPanel/Buttons/PreviewButton.jsx
@@ -0,0 +1,13 @@
+import Preview from '@/assets/svg/Eye.svg'
+
+export default function button1() {
+
+  return (
+
+    <div className='w-[48px] h-[28px] px-[8px] py-[4px] bg-[#4832A6] rounded-[4px] flex items-center justify-center'>
+      <img src={Preview} alt="" />
+    </div>
+  
+  )
+
+}

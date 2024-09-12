43a7fa0f69b801ff5ce2b3964a94f60c61960f58
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-12 07:07:45 -0400

    MDF-70

---
+++ src/components/General/MergeChat/RightPanel/Buttons/DownloadButton.jsx
@@ -0,0 +1,13 @@
+import DownloadIcon from '@/assets/svg/DownloadIcon.svg'
+
+export default function button1() {
+
+  return (
+
+    <div className='w-[48px] h-[28px] px-[8px] py-[4px] bg-[#18CBB1] rounded-[4px] flex items-center justify-center'>
+      <img src={DownloadIcon} alt="" />
+    </div>
+  
+  )
+
+}

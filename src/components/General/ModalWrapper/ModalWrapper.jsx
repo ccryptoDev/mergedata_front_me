42d5e4903c765c5fd8b847fe72155b351a5de09b
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-18 11:58:47 -0400

    MDF-44

--- src/components/General/ModalWrapper/ModalWrapper.jsx
+++ src/components/General/ModalWrapper/ModalWrapper.jsx
@@ -1,11 +1,11 @@
-const ModalWrapper = ({ children, action, onAction }) => {
-	return (
-		<div
-			onClick={() => onAction(!action)}
-			className='absolute inset-0 z-50 w-screen h-screen flex flex-col justify-center items-center bg-dark-popup-00/[.9] transition ease-in-out duration-500'
-		>
+import ReactDOM from 'react-dom';
+
+const ModalWrapper = ({ children }) => {
+	return ReactDOM.createPortal(
+		<div className='absolute left-0 inset-0 z-[999] w-screen h-screen flex flex-col justify-center items-center bg-dark-popup-00/[.9] transition ease-in-out duration-500'>
 			{children}
-		</div>
+		</div>,
+		document.getElementById('modal'),
 	);
 };
 

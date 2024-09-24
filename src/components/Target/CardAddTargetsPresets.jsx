f01f9e0ed644bcc75cbdef1decb449c479c68af0
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-24 03:27:06 -0400

    MDF-121

--- src/components/Target/CardAddTargetsPresets.jsx
+++ src/components/Target/CardAddTargetsPresets.jsx
@@ -1,19 +1,14 @@
-import RightArrowIcon from '@components/General/Icons/RightArrowIcon';
-import { usePage } from '@/hooks/usePage';
+import PlusIcon from '@/components/General/Icons/PlusIcon';
 
-export default function CardAddTargetsPresets () {
-	const { moveToPage } = usePage();
+export default function CardAddTargetsPresets() {
 	return (
 		<div className='w-[430.67px] h-[304px] bg-[#161A3E] rounded-[12px] flex flex-col items-center justify-center'>
 			<div className='mb-[6px] font-baloo font-base font-[700] text-[18px] leading-[28px] text-[#FFFFFF]'>
 				Add Target
 			</div>
 			<div>
-				<div
-					className='border-2 border-[#ffffff] rounded-full w-[55px] h-[55px] flex items-center justify-center cursor-pointer'
-					onClick={() => moveToPage('/targets/preset/add')}
-				>
-					<RightArrowIcon className='w-[13px] h-[9,5px] ' />
+				<div className='border-2 border-[#ffffff] rounded-full w-[55px] h-[55px] flex items-center justify-center cursor-pointer'>
+					<PlusIcon />
 				</div>
 			</div>
 		</div>

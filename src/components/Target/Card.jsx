fb9b68a2ea25955213db1f2131a99dfd225cd230
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-24 03:26:55 -0400

    MDF-137

--- src/components/Target/Card.jsx
+++ src/components/Target/Card.jsx
@@ -3,56 +3,51 @@ import ValueBarIncrease from '@/components/Target/ValueBarIncrease';
 import IncreaseBarName from '@/components/Target/IncreaseBarName';
 import ContainerNameIncrease from '@/components/Target/ContainerNameIncrease';
 import { usePage } from '@/hooks/usePage';
+import { useStore } from '@/store/store';
 
-const Card = ({ title, text, data, gradient, subTitle, typeOfCard }) => {
+
+const Card = ({ title, gradient, description = '', typeOfCard, subSectionData = {}, subSectionInfo = {} }) => {
+	const setSubSection = useStore(state => state.setSubSection);
 	const { moveToPage } = usePage();
+
+	const selectSection = () => {
+		setSubSection(subSectionInfo)
+		moveToPage(`/targets/${title.toLowerCase()}`)
+	}
 	return (
-		<CardGradient gradient={gradient}>
-			<div
-				className='w-[306px] h-[40px] cursor-pointer'
-				onClick={() => moveToPage(`/targets/${title.toLowerCase()}`)}
-			>
-				<h1 className='text-[28px] leading-7 font-bold text-neutrals-00'>
-					{title}
-				</h1>
-				<p
-					className={subTitle === 'na' ? 'text-xs text-neutrals-300' : 'hidden'}
-				>
-					{text}
-				</p>
-				{subTitle !== 'na' && (
-					<p className='text-sm font-bold text-white'>{subTitle}</p>
+
+		<CardGradient gradient={gradient} onClick={selectSection}>
+			<div className='w-[19.6em] h-[10.75em] flex flex-col items-center gap-y-[0.6em] justify-between '>
+				<div className='w-[19.6em] h-[3.5em]'>
+					<h1 className='text-[1.75em] leading-[1.75em] font-bold text-neutrals-00'>
+						{title}
+					</h1>
+					<p
+						className={`${description !== 'na' ? 'text-xs text-neutrals-300' : 'hidden'}`}
+					>
+						{description}
+					</p>
+				</div>
+				{typeOfCard === 1 ? (
+					<div className='flex items-center gap-[1.5em] '>
+						{subSectionData.map((item, index) => {
+							return <ValueBarIncrease item={item.values} key={`target-${index}`} />;
+						})}
+					</div>
+				) : (
+					<>
+						<div className='flex items-center h-full gap-x-[1em]'>
+							<IncreaseBarName
+								item={subSectionData[0]?.values}
+							/>
+							<IncreaseBarName
+								item={subSectionData[1]?.values}
+							/>
+						</div>
+						<ContainerNameIncrease item={subSectionData[2]?.values} item1={subSectionData[3]?.values} />
+					</>
 				)}
 			</div>
-			{typeOfCard === 1 ? (
-				<div className='flex items-center gap-6'>
-					{data.map(item => {
-						return <ValueBarIncrease {...item} key={item.dataValue} />;
-					})}
-				</div>
-			) : (
-				<>
-					<div className='flex items-center gap-6'>
-						<IncreaseBarName
-							value='204'
-							increase='down'
-							percentage='5'
-							name='Product'
-							targetValue={100}
-							realValue={40}
-						/>
-						<IncreaseBarName
-							value='603'
-							increase='up'
-							percentage='2'
-							name='Finance Reserve'
-							targetValue={60}
-							realValue={45}
-						/>
-					</div>
-					<ContainerNameIncrease />
-				</>
-			)}
 		</CardGradient>
 	);
 };

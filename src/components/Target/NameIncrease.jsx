a7b937aabf66f2845aff1deafb7ff22bd3c4936c
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-25 07:55:53 -0400

    MDF-136

--- src/components/Target/NameIncrease.jsx
+++ src/components/Target/NameIncrease.jsx
@@ -5,20 +5,21 @@ function NameIncrease ({
 	title,
 	bigPercentage,
 	smallPercentage,
-	increase,
 }) {
-	const { icon, textColor } = increaseStatus(increase);
+	const { icon, textColor } = increaseStatus(smallPercentage?.includes('-') ? 'down' : 'up');
 	return (
 		<div className='font-baloo w-[110px] h-[34px] flex flex-col items-center'>
-			<p className='font-medium text-xs text-white'>{title}</p>
-			<div className='flex'>
+			<p className='font-medium text-xs text-center text-white truncate w-[150px]'>{title}</p>
+			<div className='flex h-[20px] items-center'>
 				<p className='font-bold text-[20px] pr-1 text-white'>
-					{bigPercentage}%
+					{bigPercentage}
 				</p>
-				<img src={icon} href='increase icon' />
-				<p className={`font-bold text-[10px] ${textColor}`} >
-					{smallPercentage}%
-				</p>
+				<div className='flex items-center'>
+					<img src={icon} href='increase icon' />
+					<p className={`font-bold text-[10px] ${textColor}`} >
+						{smallPercentage}
+					</p>
+				</div>
 			</div>
 		</div>
 	);

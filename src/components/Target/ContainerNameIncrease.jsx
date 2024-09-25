97e12b0086ab10c36bf7865d96b4fe572a7646e8
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-25 07:54:29 -0400

    MDF-148

--- src/components/Target/ContainerNameIncrease.jsx
+++ src/components/Target/ContainerNameIncrease.jsx
@@ -1,20 +1,18 @@
 import NameIncrease from '@/components/Target/NameIncrease';
 
 
-function ContainerNameIncrease () {
+function ContainerNameIncrease ({ item = [], item1 = [] }) {
 	return (
-		<div className='flex justify-between rounded-lg h-[50px] w-[305px] py-2 px-6 bg-purple-700'>
+		<div className='flex justify-between rounded-lg h-[50px] w-[305px] py-2 px-6 bg-purple-700 group-hover:bg-[rgba(255,255,255,0.4)]'>
 			<NameIncrease
-				title='Product Penetration'
-				bigPercentage='13'
-				smallPercentage='3'
-				increase='down'
+				title={item[1]}
+				bigPercentage={item[0]}
+				smallPercentage={item[2]}
 			/>
 			<NameIncrease
-				title='Finance Penetration'
-				bigPercentage='19'
-				smallPercentage='6'
-				increase='up'
+				title={item1[1]}
+				bigPercentage={item1[0]}
+				smallPercentage={item1[2]}
 			/>
 		</div>
 	);

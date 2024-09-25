eb29eaca71d41030ba3eac2959d1565ba60492fa
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-25 07:54:58 -0400

    MDF-142

--- src/components/Target/Dropdown.jsx
+++ src/components/Target/Dropdown.jsx
@@ -4,44 +4,53 @@ import ArrowDownIcon from '@/components/General/Icons/ArrowDownIcon';
 import Icon from '@/components/General/Icons/Decision';
 
 export default function Dropdown ({
-	name = 'Targets',
-	width = 'w-[144px]',
-	hight = 'h-[50px]',
+	iconName = 'Target',
+	name = 'Prior Month',
+	width = 'w-[9em]',
+	hight = 'h-[3.125em]',
 	items = [
 		{
 			id: 'dd-item-0',
-			name: 'Target 1',
+			name: 'Prior Month',
 			click: () => console.log('clicked 1'),
-			selected: false,
+			selected: true,
 		},
 		{
 			id: 'dd-item-1',
-			name: 'Target 2',
+			name: 'SMLY',
 			click: () => console.log('clicked 2'),
 			selected: false,
 		},
 		{
 			id: 'dd-item-2',
-			name: 'Add Target preset',
+			name: '3 Months Avg',
 			click: () => console.log('clicked 3'),
-			selected: true,
+			selected: false,
+		},
+		{
+			id: 'dd-item-3',
+			name: 'Add Target Preset',
+			click: () => console.log('clicked 3'),
+			selected: false,
 		},
 	],
+	showOptionSelected = false
 }) {
 	const [showItems, setShowItems] = useState(false);
 
+	const nameSelected = items?.find(item => item.selected)?.name;
 	return (
-		<div className='relative'>
+		<div className='relative w-[auto]'>
 			<button
 				onClick={() => setShowItems(!showItems)}
-				className={`flex flex-row py-[12px] px-[14px] items-center ${width} ${hight} justify-between ${showItems
-						? 'border-primary-purple-600 bg-primary-purple-600'
-						: 'bg-[#05081E]'
-					} transition-colors border-[2px] border-solid border-[#212A43] shadow-[0px_13px_25px_-13px_rgba(0, 0, 0, 0.48)] rounded-[10px] hover:border-primary-purple-600 hover:bg-[#161A3E]`}
+				className={`flex flex-row py-[0.75em] px-[0.875em] items-center ${width} ${hight} justify-between ${showItems
+					? 'border-primary-purple-600 bg-primary-purple-600'
+					: 'bg-[#05081E]'
+					} transition-colors border-[2px] border-solid border-[#212A43] shadow-[0px_13px_25px_-13px_rgba(0, 0, 0, 0.48)] rounded-md hover:border-primary-purple-600 hover:bg-[#161A3E]`}
 			>
-				<Icon name={name} />
+				<Icon name={iconName} />
 				<span className='font-[600] font-baloo text-[15px] leading-[26px] text-[#EEECF1]'>
-					{name}
+					{showOptionSelected ? nameSelected : name}
 				</span>
 				<ArrowDownIcon
 					className={`duration-300 ${showItems ? 'rotate-180' : ''}`}
@@ -52,11 +61,14 @@ export default function Dropdown ({
 					{items.map(item => (
 						<button
 							key={item.id}
-							className={`w-full h-[32px] font-baloo font-[600] text-sm text-[#FFFFFF] ${item.selected
-									? 'text-center bg-primary-purple-600 rounded-[5px] px-2'
-									: 'px-3 text-start'
+							className={`w-full h-[2em] font-baloo font-[600] text-sm text-[#FFFFFF] text-center ${item.selected
+								? 'bg-primary-purple-600 rounded-[0.3125em] px-2'
+								: 'px-3'
 								}`}
-							onClick={item.click}
+							onClick={() => {
+								item.click();
+								setShowItems(false);
+							}}
 						>
 							{item.name}
 						</button>

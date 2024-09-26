b9527c8aafe9b17954f977f8a639b2d129a93b1e
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-26 09:26:35 -0400

    MDF-134

--- src/components/Target/TitleVectorX.jsx
+++ src/components/Target/TitleVectorX.jsx
@@ -1,4 +1,4 @@
-import vector from '@/assets/svg/vector.svg'
+import Decision from "@/components/General/Icons/Decision";
 
 export default function TitleVectorX({
     width = 'w-96',
@@ -13,20 +13,22 @@ export default function TitleVectorX({
     mainTitleColor = 'text-neutrals-00',
     upperTitleFont = 'font-baloo',
     mainTitleFont = 'font-baloo',
+    onClick,
 }) {
+
     return (
-        <div className={`${width} ${height} flex flex-row justify-between border-blue-500 shadow-md`}>
+        <div className={`${width} ${height} flex flex-row justify-between items-start px-[0.5em] `}>
             <div className='w-full h-full flex flex-col content-between' >
-                <div className='mb-[4px] w-full h-[12px]' >
+                <div className='mb-[0.25em] w-full h-[1em]' >
                     <p className={`${upperTitleSize} ${upperTitleWeigth} ${upperTitleColor} ${upperTitleFont}`}>{upperTitle}</p>
                 </div>
-                <div className='w-full h-[23px]' >
+                <div className='w-full h-[1.4375em]' >
                     <p className={`${mainTitleSize} ${mainTitleWeigth} ${mainTitleColor} ${mainTitleFont}`}>{mainTitle}</p>
                 </div>
             </div>
-            <div className='w-[14px] h-[14px] flex flex-col justify-center items-center' >
-                <img src={vector} className='w-[14px] h-[14px]' />
-            </div>
+            <button onClick={onClick} className='w-[1.5em] h-[1.5em] flex justify-center items-center hover:scale-110'>
+                <Decision name='Cancel' />
+            </button>
         </div>
     )
 }

e816196bd7ccfe41342c704c863319ea49253d2d
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-10-08 06:10:06 -0400

    MDF-216

--- src/components/Target/New/NameIncreaseDropdown.jsx
+++ src/components/Target/New/NameIncreaseDropdown.jsx
@@ -1,7 +1,6 @@
 import Dropdown from '@/components/Target/Dropdown';
 import { increaseStatus } from '@/utils/helperFunctions';
 
-
 const NameIncreaseDropdown = ({
 	title = 'Financial Statement',
 	value = '6,466',
@@ -10,23 +9,23 @@ const NameIncreaseDropdown = ({
 }) => {
 	const { icon, textColor } = increaseStatus(increase);
 	return (
-		<div className='w-[228px] h-[78px] flex flex-col'>
+		<div className='h-[auto] mb-2 flex flex-col'>
 			<div className='flex'>
-				<h4 className='w-[217px] h-[36px] font-baloo font-bold text-[24px] text-white'>
+				<h4 className='h-[36px] font-baloo font-bold text-[24px] text-white'>
 					{title}
 				</h4>
 			</div>
 			<div className='h-[42px] flex justify-between items-center'>
-				<div className='flex justify-between w-[124px]'>
-					<p className='font-baloo text-white text-[32px] font-bold'>{value}</p>
+				<div className='flex gap-[6%]'>
+					<p className='font-baloo text-white text-[32px] font-bold tall:text-[22px]'>{value}</p>
 					<div className='flex items-center justify-center'>
 						<img src={icon} className='w-[15px] h-[12px]' />
-						<p className={`${textColor} font-baloo font-bold text-[14px]`}>
+						<p className={`${textColor} font-baloo font-bold text-[14px] tall:text-[10px]`}>
 							{percentage}
 						</p>
 					</div>
 				</div>
-				<Dropdown name='Units' hight='h-[42px]' width='w-[88px]' />
+				<Dropdown name='Units' hight='h-[42px]' width='w-[80%]' />
 			</div>
 		</div>
 	);

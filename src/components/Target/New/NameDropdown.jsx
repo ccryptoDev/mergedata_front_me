64bf4813ed5f0d03c4a62db7cb5d6b923dd85b63
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-10-08 06:09:56 -0400

    MDF-213

--- src/components/Target/New/NameDropdown.jsx
+++ src/components/Target/New/NameDropdown.jsx
@@ -3,17 +3,16 @@ import Dropdown from '@/components/Target/Dropdown';
 const NameDropdown = ({
 	title = 'Salesperson',
 	dropDownText = 'Top 3',
-	width = 'w-[244px]',
-	margin = 'mb-[20px]',
+	width = 'w-[15.25em]',
+	margin = 'mb-[1.25em]',
 }) => {
 	return (
 		<div
-			className={`${width} h-[42px] flex justify-between text-center items-center ${margin}`}
+			className={`${width} h-[2.625em] flex justify-between text-center items-center ${margin} `}
 		>
-			<h4 className='font-baloo font-bold text-white text-[24px]'>{title}</h4>
-			<div className='mr-[18px]'>
-				<Dropdown name={`${dropDownText}`} hight='h-[42px]' width='w-[87px]' />
-			</div>
+			<h4 className='font-baloo font-bold text-white text-[1.5em] mr-[0.5em]'>{title}</h4>
+
+			<Dropdown name={`${dropDownText}`} hight='h-[2.3em]' width='w-[70%]' iconName = '' />
 		</div>
 	);
 };

8f09b34f4162b11348c7ea95141d88e52012a2af
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-10-08 06:09:37 -0400

    MDF-214

--- src/components/Target/New/ModelsTable2.jsx
+++ src/components/Target/New/ModelsTable2.jsx
@@ -3,13 +3,31 @@ import IconNameIconDropdownButton from '@/components/General/Buttons/IconNameIco
 import SingleButton from '@/components/General/Buttons/SingleButton';
 import Table5 from '@/components/Target/New/Table5';
 
-export default function ModelsTable2() {
-    const [reduce, setReduce] = useState(false);
+export default function ModelsTable2 ({
+    reduceTable,
+    showComments,
+    showLargeTable
+}) {
+
+    // const [shouldReduce, setShouldReduce] = useState(false)
+    const [shouldReduce,] = useState(false)
+    // const [shouldComment, setShouldComment] = useState(false)
+
+    // const handleShouldReduce = () => {
+    //     setShouldReduce(!shouldReduce)
+    //     reduceTable(shouldReduce)
+    // }
+
+    // const handleShowCommentsCard = () => {
+    //     setShouldComment(!shouldComment)
+    //     showComments(shouldComment)
+    //     setShouldReduce(false)
+    // }
 
     return (
 
-        <div className={`bg-neutrals-900 px-[26px] pt-[20px] ${!reduce ? 'w-[962px]' : 'w-[661px]'}  h-[666px] rounded-2xl`}>
-            <div className='h-[50px] w-full border-neutrals-700 flex flex-row items-center justify-between'>
+        <div className={`bg-neutrals-900 px-[24px] pt-[20px] ${(shouldReduce && !showLargeTable) ? 'w-[691px]' : 'w-[1004px]'} h-[666px] rounded-2xl`}>
+            <div className='h-[50px] w-full flex flex-row items-center justify-between'>
                 <div className='flex flex-row gap-x-3'>
                     <SingleButton />
                     <IconNameIconDropdownButton
@@ -17,7 +35,7 @@ export default function ModelsTable2() {
                         iconName=''
                         background='bg-neutrals-800'
                         width='w-[128px]'
-                        border='border border-[2px] border-neutrals-900'
+                        border='border border-neutrals-900'
                         height='h-[50px]'
                         backgroundHover='hover:bg-neutrals-900 transition duration-300 delay-150 hover:delay-300'
                         borderHover='hover:border-purple-600'
@@ -25,7 +43,7 @@ export default function ModelsTable2() {
                     />
                 </div>
                 <div className='flex flex-row gap-x-2'>
-                    <SingleButton
+                    {/* <SingleButton
                         icon='Share'
                         background=''
                         width='w-[50px]'
@@ -34,35 +52,9 @@ export default function ModelsTable2() {
                         backgroundHover='hover:bg-purple-300 transition duration-100 delay-150 hover:delay-300'
                         borderHover='hover:border-purple-100 rounded-full hover:outline-offset-4 hover:opacity-50'
                         radius=''
-                    />
-                    <button onClick={() => setReduce(!reduce)}>
-                    {reduce ?
-                            <SingleButton
-                                icon='Pause'
-                                background='bg-purple-100 bg-opacity-70'
-                                width='w-[50px]'
-                                border='ring ring-purple-100 ring-opacity-70 border-[4px] border-neutrals-900'
-                                height='h-[50px]'
-                                backgroundHover='hover:bg-purple-300 bg-opacity-70 transition duration-100 delay-150 hover:delay-300'
-                                borderHover='hover:border-purple-100 rounded-full hover:outline-offset-4 hover:opacity-50'
-                                radius=''
-                            />
-                            :
-                            <SingleButton
-                                icon='Pause'
-                                background=''
-                                width='w-[50px]'
-                                border=''
-                                height='h-[50px]'
-                                backgroundHover='hover:bg-purple-300 transition duration-100 delay-150 hover:delay-300'
-                                borderHover='hover:border-purple-100 rounded-full hover:outline-offset-4 hover:opacity-50'
-                                radius=''
-                            />
-                        }
-                    </button>
-
+                    /> */}
                     <SingleButton
-                        icon='Chat'
+                        icon='Download'
                         background=''
                         width='w-[50px]'
                         border=''
@@ -71,12 +63,37 @@ export default function ModelsTable2() {
                         borderHover='hover:border-purple-100 rounded-full hover:outline-offset-4 hover:opacity-50'
                         radius=''
                     />
+                    {/* <SingleButton
+                        icon='Pause'
+                        background=''
+                        width='w-[50px]'
+                        border=''
+                        height='h-[50px]'
+                        backgroundHover='hover:bg-purple-300 bg-opacity-70 transition duration-100 delay-150 hover:delay-300'
+                        borderHover='hover:border-purple-100 rounded-full hover:outline-offset-4 hover:opacity-50'
+                        radius=''
+                        onClick={handleShouldReduce}
+                    /> */}
+                    {/* <SingleButton
+                        icon='Chat'
+                        background=''
+                        width='w-[50px]'
+                        border=''
+                        height='h-[50px]'
+                        backgroundHover='hover:bg-purple-300 transition duration-100 delay-150 hover:delay-300'
+                        borderHover='hover:border-purple-100 rounded-full hover:outline-offset-4 hover:opacity-50'
+                        radius=''
+                        onClick={handleShowCommentsCard}
+                    /> */}
                 </div>
             </div>
             <div className='w-full'>
-                <div className="inline-block min-w-full py-2 sm:px-0 lg:px-4">
+                <div className="inline-block min-w-full py-2 ">
                     <div className="overflow-hidden">
-                        <Table5 shouldReduce={reduce} />
+                        <Table5
+                            shouldReduce={shouldReduce}
+                            showLargeTable={showLargeTable}
+                        />
                     </div>
                 </div>
             </div>

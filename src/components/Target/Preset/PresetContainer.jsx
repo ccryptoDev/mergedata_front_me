afb5527008555033eaffff424a2f66ecc479ad46
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-10-11 06:03:53 -0400

    MDF-143

--- src/components/Target/Preset/PresetContainer.jsx
+++ src/components/Target/Preset/PresetContainer.jsx
@@ -1,6 +1,6 @@
 function PresetContainer({ children }) {
 	return (
-		<div className='w-[1340px] h-[602px] bg-[#0D102C] rounded-3xl'>
+		<div className='w-[83.75em] h-[602px] bg-[#0D102C] rounded-3xl flex justify-center items-center'>
 			{children}
 		</div>
 	);

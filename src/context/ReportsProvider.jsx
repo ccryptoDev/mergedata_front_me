243df24730ad633497c3d14fb77a49fdc6e9a549
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-10-17 06:26:52 -0400

    MDF-241

--- src/context/ReportsProvider.jsx
+++ src/context/ReportsProvider.jsx
@@ -3,32 +3,70 @@ import { createContext, useState } from 'react';
 const ReportsContext = createContext({});
 
 export const ReportsProvider = ({ children }) => {
-    const [section, setSection] = useState({
-        id: 1,
-    });
-    const [subSection, setSubSection] = useState({ clientId: 1, id: 4 });
-    const [report, setReport] = useState({
-        id: 1,
-        clientId: 1,
-        storeId: [1],
-        period: ['2023-04'],
-    });
-    const [sections, setSections] = useState([{
-        id: 1,
-    }]);
-    const [subSections, setSubSections] = useState([{ clientId: 1, id: 4 }]);
-    const [reports, setReports] = useState([{
-        id: 1,
-        clientId: 1,
-        storeId: [1],
-        period: ['2023-04'],
-    }]);
+	const [user, setUser] = useState({
+		clientSelected: 1,
+		storesSelected: [1],
+		stores: [1],
+		clients: [1],
+		userId: 0,
+		firstName: '',
+		lastName: '',
+		email: ''
+	});
+	const [section, setSection] = useState({ id: 0 });
+	const [subSection, setSubSection] = useState({ clientId: 1, id: 0 });
+	const [report, setReport] = useState({
+		isYTD: true,
+		byTrend: false,
+		id: 0,
+		clientId: 1,
+		storeId: [1],
+		period: null,
+		sourceOfReport: '',
+	});
+	const [reportFormatted, setReportFormatted] = useState(null);
 
-    return (
-        <ReportsContext.Provider value={{ section, setSection, subSection, setSubSection, report, setReport, sections, setSections, subSections, setSubSections, reports, setReports }}>
-            {children}
-        </ReportsContext.Provider>
-    );
+	const [sections, setSections] = useState([]);
+	const [subSections, setSubSections] = useState([]);
+	const [reports, setReports] = useState([]);
+
+	const [target, setTarget] = useState([{ id: 1, name: 'Prior Month', label: 'Prior Month' }]);
+	const [stores, setStores] = useState([]);
+	const [storesSelected, setStoresSelected] = useState(null);
+	const [selectedStoreGroups, setSelectedStoreGroups] = useState(null);
+
+	return (
+		<ReportsContext.Provider
+			value={{
+				user,
+				setUser,
+				section,
+				setSection,
+				subSection,
+				setSubSection,
+				report,
+				setReport,
+				reportFormatted,
+				setReportFormatted,
+				sections,
+				setSections,
+				subSections,
+				setSubSections,
+				reports,
+				setReports,
+				target,
+				setTarget,
+				stores,
+				setStores,
+				storesSelected,
+				setStoresSelected,
+				selectedStoreGroups,
+				setSelectedStoreGroups,
+			}}
+		>
+			{children}
+		</ReportsContext.Provider>
+	);
 };
 
 export default ReportsContext;
